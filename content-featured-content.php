18b3cbdd35f057b12d0b6bfe6af695b41d5a7fd4
Adrian Pillinger <adrian.pillinger@gmail.com>
2013-07-02 12:37:57 +0100

    Adding and styling the featured images section

---
+++ content-featured-content.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+<section class="featured-content">
+	<div class="featured-content">
+		<div class="main-feature">
+			<a href="/?page_id=66"><img alt="I'm new here" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/church-building.jpg"></a>
+		</div>
+		<div class="secondary-features">
+			<ul>
+				<li class="top"><a href="/?page_id=25"><img alt="Armor of God" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/armour-of-god-light.jpg"></a></li>
+				<li class="bottom"><a href="/?p=100"><img alt="Recommended books" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/recommended-books.jpg"></a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+
