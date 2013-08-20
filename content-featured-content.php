0f251014c3d8c30d9da9b3253cea2fae493ff5e2
Adrian Pillinger <adrian.pillinger@gmail.com>
2013-08-20 21:08:33 +0100

    Adding titles to feature images

--- content-featured-content.php
+++ content-featured-content.php
@@ -11,14 +11,36 @@
 <section class="featured-content">
 	<div class="featured-content">
 		<div class="main-feature">
-			<a href="/?page_id=66"><img alt="I'm new here" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/church-building.jpg"></a>
+			<a href="/?page_id=66">
+				<img alt="I'm new here" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/church-building.jpg">
+				<div class="title">I'm new here</div>
+			</a>
 		</div>
 		<div class="secondary-features">
 			<ul>
-				<li class="top"><a href="/?page_id=25"><img alt="Corsham's Got Talent" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/corshams-got-talent.jpg"></a></li>
-				<li class="bottom"><a href="/?p=100"><img alt="Recommended books" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/recommended-books.jpg"></a></li>
+				<li class="top">
+					<a href="/?page_id=25">
+						<img alt="Corsham's Got Talent" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/corshams-got-talent.jpg">
+						<div class="title">Corsham's Got Talent</div>
+					</a>
+				</li>
+				<li class="bottom">
+					<a href="/?p=100">
+						<img alt="Recommended books" src="<?php echo get_bloginfo('stylesheet_directory');?>/images/recommended-books.jpg">
+						<div class="title">Recommended books</div>
+					</a>
+				</li>
 			</ul>
 		</div>
 	</div>
 </section>
+<!-- 
 
+<div style="
+    font-family: Futura;
+    text-transform: uppercase;
+    text-align: right;
+    margin: auto 0;
+    margin-bottom: 20px;
+">I'm new here</div>
+ -->
